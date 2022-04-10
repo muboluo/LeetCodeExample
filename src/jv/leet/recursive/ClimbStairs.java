009c9945e64214d5986940d2b47830f341ff2896
zhangheng24 <zhangheng24@58.com>
2022-04-10 17:05:44 +0800

    Update ClimbStairs.java

--- src/jv/leet/recursive/ClimbStairs.java
+++ src/jv/leet/recursive/ClimbStairs.java
@@ -9,43 +9,50 @@ package jv.leet.recursive;
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  * <p>
  * 解题思路：
+ * <p>
  * 本题目是常见的动态规划的题目
  * 我们可以把他分解成多个子问题，来进行求解。
+ * <p>
  * 我们设爬到第n个台阶的方法数的求分级方法 为 f(n)
  * 则爬到第n个台阶的最后一步一共有两种方法，
  * 分别是最后跨一个台阶和 跨两个台阶。
+ * <p>
  * 分别是从第 n-1个台阶 和 第n-2个台阶爬上来的。
  * 爬n个台阶的方法为f(n),因此，我们可以推导出 n-1 和 n-2个台阶的方法
  * f(n-1) 和 f(n-2)
+ * <p>
  * 由此，我们可以推导出。f(n) = f(n-1) + f(n-2)
  * 因此我们可以采用for 或者递归的形式进行解答。
- * 但我再提交 leetcode的时候，采用递归的方式提交总是报超时的错误。
+ * <p>
+ * 但我在提交 leetcode 的时候，采用递归的方式提交总是报超时的错误。
  * 所以我们这里采用的for 循环的结题方式提交的答案。
  * 不过，在这里，仍将递归的答案列在这里。
  */
 
 public class ClimbStairs {
 
-    public int climbStairsRecursive(int n) {
+    // 通过递归的方式，处理 - 第一遍
+    public static int climbStairsRecursive(int n) {
 
-        //terminal
+        //terminal ，递归到最后一级的结果。
         if (n <= 1) {
             return 1;
         } else if (n <= 2) {
             return n;
         }
 
-        //current level handle
+        //current level handle， 处理当前层级的实现。
         int result = climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
         //jump next level
 
-        // reset clause
+        // reset clause 重置递归条件
 
         return result;
 
     }
 
-    public int climbStairsArray(int n) {
+    // 通过数组的方式处理 - 第一遍
+    public static int climbStairsArray(int n) {
 
         if (n <= 1) {
             return 1;
@@ -69,11 +76,11 @@ public class ClimbStairs {
 
     public static void main(String[] args) {
 
-        int result = new ClimbStairs().climbStairsRecursive(10);
+        int result = climbStairsRecursive(10);
 
         System.out.println(result);
 
-        int result2 = new ClimbStairs().climbStairsArray(10);
+        int result2 = climbStairsArray(10);
 
         System.out.println(result2);
 
