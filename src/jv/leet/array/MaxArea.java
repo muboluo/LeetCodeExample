21303348f3d69851c725f2db60e2111f3d74a5ca
zhangheng24 <zhangheng24@58.com>
2022-04-10 16:07:07 +0800

    Update MaxArea.java

--- src/jv/leet/array/MaxArea.java
+++ src/jv/leet/array/MaxArea.java
@@ -11,8 +11,10 @@ package jv.leet.array;
  * 链接：https://leetcode-cn.com/problems/container-with-most-water
  * <p>
  * key: 如果我们试图将指向较长线段的指针向内移动，移动后，新形成的矩形，受限于先前较小的线段的概率会更大。
+ * <p>
  * 目标：我们的目标是寻找新的受限条件，而不是替换不受限的条件。
  * 我们只有在X轴不断减小的同时，寻找更高的Y轴线段才能获得更大的矩形。
+ * <p>
  * https://leetcode-cn.com/problems/container-with-most-water/solution/container-with-most-water-shuang-zhi-zhen-fa-yi-do/
  * 这篇文章分析的很不错，讲到了消去所有的状态。
  * <p>
@@ -71,9 +73,33 @@ class MaxArea {
 
         final int result = maxArea(new int[]{1, 2, 3, 4, 5, 6});
         final int result2 = maxArea2(new int[]{1, 2, 3, 4, 5, 6});
+        final int result3 = maxArea3(new int[]{1, 2, 3, 4, 5, 6});
 
         System.out.println(result);
         System.out.println(result2);
+        System.out.println(result3);
+    }
+
+    // 第三遍
+    private static int maxArea3(int[] ints) {
+
+        int result = 0;
+        if (ints == null) {
+            return result;
+        }
+
+        int left = 0, right = ints.length - 1;
+
+        while (left < right) {
+            result = Math.max(result, (right - left) * Math.min(ints[right], ints[left]));
+
+            if (ints[left] < ints[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
     }
 
 }
