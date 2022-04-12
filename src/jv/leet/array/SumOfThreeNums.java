f1a7526c60017a1a71714e8886f95a55fbec3510
zhangheng24 <zhangheng24@58.com>
2022-04-13 00:20:30 +0800

    Update SumOfThreeNums.java

--- src/jv/leet/array/SumOfThreeNums.java
+++ src/jv/leet/array/SumOfThreeNums.java
@@ -11,26 +11,34 @@ import java.util.List;
  * 注意：答案中不可以包含重复的三元组。
  * <p>
  * 来源：力扣（LeetCode）
+ * <p>
  * 链接：https://leetcode-cn.com/problems/3sum
  * <p>
  * 解题思路：
+ * <p>
  * 标签：数组遍历
+ * <p>
  * 首先对数组进行排序，排序后固定一个数 nums[i]nums[i]，再使用左右指针指向 nums[i]nums[i]后面的两端，数字分别为 nums[L]nums[L] 和 nums[R]nums[R]，计算三个数的和
  * sumsum 判断是否满足为 00，满足则添加进结果集
+ * <p>
  * 如果 nums[i]nums[i]大于 00，则三数之和必然无法等于 00，结束循环
  * 如果 nums[i]nums[i] == nums[i-1]nums[i−1]，则说明该数字重复，会导致结果重复，所以应该跳过
+ * <p>
  * 当 sumsum == 00 时，nums[L]nums[L] == nums[L+1]nums[L+1] 则会导致结果重复，应该跳过，L++
  * 当 sumsum == 00 时，nums[R]nums[R] == nums[R-1]nums[R−1] 则会导致结果重复，应该跳过，R--
+ * <p>
  * 时间复杂度：O(n^2)，n 为数组长度
  */
-class FifthSumOfThreeNums {
+class SumOfThreeNums {
 
     public static void main(String[] args) {
 
+        threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
 
+        // 1 terminal clause
         List<List<Integer>> ans = new ArrayList<>();
         if (nums == null) {
             return ans;
@@ -41,42 +49,55 @@ class FifthSumOfThreeNums {
             return ans;
         }
 
+        // 2 排序
         Arrays.sort(nums);
 
         int l = 0;
         int r = 0;
 
+        // 3 loop current
         for (int i = 0; i < len; ++i) {
+            // 4. terminal clause
             if (nums[i] > 0) {
-                return ans;
+                break;
             }
 
+            // 5. jump next i
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
+            // 6. handle current level
             l = i + 1;
             r = len - 1;
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
                 if (sum == 0) {
                     ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    // 7. jump next level
                     while (l < r && nums[l] == nums[l + 1]) {
                         l++;
                     }
                     while (l < r && nums[r] == nums[r - 1]) {
                         r--;
                     }
+                    l++;
+                    r--;
                 } else if (sum < 0) {
+                    // 8. jump next level from left
                     l++;
                 } else if (sum > 0) {
+                    // 9. jump next level from right
                     r--;
                 }
             }
 
         }
 
+        for (List<Integer> ints :
+                ans) {
+            System.out.println(Arrays.toString(ints.toArray()));
+        }
         return ans;
-
     }
 }
