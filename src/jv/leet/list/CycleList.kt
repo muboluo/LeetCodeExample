6471016008f1fa712ea8f90c4d8a58a635284342
muboluo <muboluo@126.com>
2022-04-27 23:28:01 +0800

    Create CycleList.kt

---
+++ src/jv/leet/list/CycleList.kt
@@ -0,0 +1,43 @@
+package jv.leet.list
+/**
+ * 环形链表
+ * 给你一个链表的头结点head,判断链表中是否有环。
+ * <p>
+ * https://leetcode-cn.com/problems/linked-list-cycle/solution/yi-wen-gao-ding-chang-jian-de-lian-biao-wen-ti-h-2/
+ * <p>
+ * 连接中，这个一文搞定的思路很牛x。
+ * 这是一个相遇问题, 如果存在环，则快慢指针一定会相遇
+ */
+fun cycle(node: ListNode?): Boolean {
+
+    var fast = node
+    var slow = node
+
+    while (fast != null) {
+        fast = fast.next
+        if (fast == null) {
+            return false
+        }
+        fast = fast.next
+
+        if (fast == slow) {
+            return true
+        }
+        slow = slow?.next
+    }
+
+    return false
+}
+
+fun main() {
+
+    val node1 = ListNode()
+    val node2 = ListNode(node1)
+    val node3 = ListNode(node2)
+    val node4 = ListNode(node3)
+    val node5 = ListNode(node4)
+    val node6 = ListNode(node5)
+    val node7 = ListNode(node6)
+    node1.next = node4
+    System.out.println(cycle(node7))
+}
