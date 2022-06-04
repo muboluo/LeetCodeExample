d7ca734992ab34209310ca48ef82479aa21d005d
muboluo <muboluo@126.com>
2022-06-04 11:29:51 +0800

    Update DeleteRepeatInOrderedArray.java

--- src/jv/leet/array/DeleteRepeatInOrderedArray.java
+++ src/jv/leet/array/DeleteRepeatInOrderedArray.java
@@ -2,6 +2,13 @@ package jv.leet.array;
 
 import java.util.Arrays;
 
+
+/**
+ * 删除有序数组中的重复项
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/solution/shuang-zhi-zhen-shan-chu-zhong-fu-xiang-dai-you-hu/
+ * <p>
+ * 步骤：
+ */
 public class DeleteRepeatInOrderedArray {
 
 
@@ -41,13 +48,32 @@ public class DeleteRepeatInOrderedArray {
             fast++;
         }
         System.out.println(Arrays.toString(array));
-        return 0;
+        return slow;
+    }
+
+    public static int deleteRepeat3(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 0;
+        while (fast < array.length) {
+
+            if (array[slow] != array[fast]) {
+                array[++slow] = array[fast];
+            }
+            fast++;
+        }
+
+        System.out.println(Arrays.toString(array));
+        return slow;
+
     }
 
     public static void main(String[] args) {
 
         deleteRepeat(new int[]{0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 7, 9});
         deleteRepeat2(new int[]{0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 7, 9});
+        deleteRepeat3(new int[]{0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 7, 9});
 
     }
 }
