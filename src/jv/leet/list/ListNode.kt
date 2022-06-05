012ce89533fb6c3840e542f2a9376b53aa4422c7
muboluo <muboluo@126.com>
2022-06-05 23:15:59 +0800

    Update ListNode.kt

--- src/jv/leet/list/ListNode.kt
+++ src/jv/leet/list/ListNode.kt
@@ -7,7 +7,7 @@ class ListNode(var next: ListNode? = null, var value: String = "") {
         fun printNode(node: ListNode?) {
             var print = node
             while (print != null) {
-                System.out.print(print)
+                System.out.print(print.value)
                 System.out.print(" ")
                 print = print.next
             }
