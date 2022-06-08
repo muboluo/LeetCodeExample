a60658babca7f316c86ff77dfa03b4180563fffd
muboluo <muboluo@126.com>
2022-06-08 09:27:58 +0800

    Create ValidBrackets.kt

---
+++ src/jv/leet/list/ValidBrackets.kt
@@ -0,0 +1,26 @@
+package jv.leet.list
+
+/**
+ * 是否是有效括号
+ *
+ * https://leetcode.cn/problems/valid-parentheses/
+ *
+ *
+ */
+class ValidBrackets {
+
+    companion object {
+
+        fun valid(str: String): Boolean {
+
+
+            return false
+        }
+
+    }
+}
+
+fun main() {
+
+
+}
