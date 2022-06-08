e6ed71677e17b7f1e9dc76bb9110429d286ab182
muboluo <muboluo@126.com>
2022-06-09 00:27:12 +0800

    Update ValidBrackets.kt

--- src/jv/leet/list/ValidBrackets.kt
+++ src/jv/leet/list/ValidBrackets.kt
@@ -1,20 +1,75 @@
 package jv.leet.list
 
+import java.util.*
+
 /**
  * 是否是有效括号
  *
  * https://leetcode.cn/problems/valid-parentheses/
  *
+ * 解法1： 入栈，出栈
  *
  */
 class ValidBrackets {
 
     companion object {
 
-        fun valid(str: String): Boolean {
+        fun valid11(str: String?): Boolean {
 
+            if (str == "" || str == null) {
+                return false
+            }
 
-            return false
+            var stack = Stack<Char>()
+
+            str.let {
+
+                for (c in it.toCharArray()) {
+
+                    if (c == '{') {
+
+                        stack.push('}')
+                    } else if (c == '[') {
+
+                        stack.push(']')
+                    } else if (c == '(') {
+                        stack.push(')')
+                    } else if (stack.isEmpty() || stack.pop() != c) {
+                        return false
+                    }
+                }
+
+            }
+
+            return stack.isEmpty()
+        }
+
+        fun valid12(str: String?): Boolean {
+
+            if (str == null || str == "") {
+                return false
+            }
+
+            var stack = Stack<Char>()
+
+            str.let {
+
+                for (c in it.toCharArray()) {
+
+                    if (c == '{') {
+                        stack.push('}')
+                    } else if (c == '(') {
+                        stack.push(')')
+                    } else if (c == '[') {
+                        stack.push(']')
+                    } else if (stack.isEmpty() || c != stack.pop()) {
+                        return false
+                    }
+
+                }
+            }
+
+            return stack.isEmpty()
         }
 
     }
@@ -22,5 +77,10 @@ class ValidBrackets {
 
 fun main() {
 
+    System.out.println(ValidBrackets.valid11("{{{}}}(())[][]"))
+    System.out.println(ValidBrackets.valid11("{{{}}}(())[]["))
+
+    System.out.println(ValidBrackets.valid12("{{{}}}(())[][]"))
+    System.out.println(ValidBrackets.valid12("{{{}}}(())[]["))
 
 }
